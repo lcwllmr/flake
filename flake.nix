ad9174ccb7dae3da08a3a3a7e55f168979f98d3f
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-03-16 15:51:29 +0100

    add home-manager and fish dummy module

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     disko.url = "github:nix-community/disko";
     impermanence.url = "github:nix-community/impermanence";
   };
@@ -11,12 +12,18 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.home-manager.flakeModules.home-manager
+      ];
       flake = {
         nixosModules = {
           core = import ./modules/nixos/core { inherit inputs; };
         };
+        homeModules = {
+          fish = import ./modules/home-manager/fish.nix { inherit inputs; };
+        };
       };
-      imports = [ inputs.treefmt-nix.flakeModule ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
