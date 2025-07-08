c2098d3e8e0855a8e9bf6e127df4aea7d108e482
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-07-08 15:12:53 +0200

    t490s: add uv

--- configs/home/t490s.nix
+++ configs/home/t490s.nix
@@ -1,8 +1,13 @@
+{ pkgs, ... }:
 {
   home.username = "lcwllmr";
   home.homeDirectory = "/home/lcwllmr";
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    uv
+  ];
 
   imports = [
     ../../modules/home/fish.nix
