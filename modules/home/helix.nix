be14fa16e82704bcd7ef8adede472af42ebc93ad
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-07-11 19:06:58 +0200

    add helix home module

---
+++ modules/home/helix.nix
@@ -0,0 +1,15 @@
+{
+  programs.helix = {
+    enable = true;
+    settings = {
+      editor = {
+        line-number = "relative";
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+      };
+    };
+  };
+}
