f210224c4ea7faa63609be7c053d35a8d12d8938
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-07-31 09:55:46 +0200

    helix: add typst and tinymist

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    typst
+    tinymist
+  ];
+
   programs.helix = {
     enable = true;
     settings = {
@@ -13,15 +18,32 @@
       };
     };
     languages = {
-      language-server.ruff = with pkgs; {
-        command = "${ruff}/bin/ruff";
-        args = ["server"];
+      language-server = {
+        ruff = with pkgs; {
+          command = "${ruff}/bin/ruff";
+          args = ["server"];
+        };
+        tinymist = {
+          command = "tinymist";
+          config = {
+            preview.background.enabled = true;
+            preview.background.args = [
+              "--data-plane-host=127.0.0.1:23635"
+              "--invert-colors=never"
+              "--open"
+            ];
+          };
+        };
       };
       language = [
         {
           name = "python";
-          language-servers = ["ruff"];
+          language-servers = [ "ruff" ];
           auto-format = true;
+        }
+        {
+          name = "typst";
+          language-servers = [ "tinymist" ];
         }
       ];
     };
